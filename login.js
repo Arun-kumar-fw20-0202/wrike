dd53a3df4255394f596a0b24fc7a8daf90567c08
Arun Kumar <arun.kumar97107@gmail.com>
2022-08-27 21:25:52 +0530

    style page modifyed

---
+++ login.js
@@ -0,0 +1,42 @@
+
+var login_data = JSON.parse(localStorage.getItem('user_details'))
+
+document.querySelector('#emailFm').addEventListener("submit",function(e) {
+    e.preventDefault();
+    var user_email = document.querySelector('.user_email').value;
+    var new_user = user_email.trim();
+    
+    var backBtn = document.querySelector('.box + p').innerHTML = '<span class="back"> Back <span>';
+    
+    document.querySelector('.back').addEventListener('click',function(){    
+        var backBtn = document.querySelector('.box + p').innerHTML = "Don't have a Wrike account yet? <a href='signup.html'>Get started now</a>";
+        document.querySelector('.details').innerHTML = '<form id="emailFm"><div class="inputBx"><input type="text" class="user_email" placeholder="Email or Corporate ID"></div><div class="inputBx"><input type="submit" class="next" value="Next"></div><div class="forg"><a href="#">Forget password?</a></div><div class="other"><span>or log in with</span><div class="icons"><span><i class="fa fa-google"></i> Google</span><span><i class="fa fa-apple"></i> Signup with Apple</span><span><i class="fa-brands fa-microsoft"></i> Office</span></div></div></form>';
+    })
+
+    if(new_user != ""){
+        document.querySelector('.details').innerHTML = '<form id="passwordFm"><div class="data2"><span>Enter your password</span><small>'+" "+user_email+" "+' <a href="#">Wrong email?</a></small></div><div class="inputBx"><input type="password" class="password" placeholder="Password"></div><div class="inputBx"><input type="submit" value="Log in"></div><div class="forg"><a href="#">Forget password?</a></div></form>';
+    }else{
+        alert("Please fill this field")
+    }
+
+
+
+    document.querySelector('#passwordFm').addEventListener("submit",function(e){
+        e.preventDefault();
+        var sucess;
+        var password = document.querySelector(".password").value;
+
+            for(let i = 0; i<= login_data.length-1; i++){
+                var ele = login_data[i];
+                if(ele.email == new_user && ele.password == password){
+                    window.location.href = 'project.html';
+                    break;
+                }else{
+                    alert("user not found")
+                    break;
+                }
+            }
+        })
+})
+
+
---
+++ signup.js
@@ -0,0 +1,23 @@
+
+var count = 0;
+var user_data = JSON.parse(localStorage.getItem('user_data')) || [];
+document.querySelector('form').addEventListener('submit',function(e){
+    e.preventDefault();
+
+    var name = document.querySelector('.name').value;
+    var password = document.querySelector('.password').value;
+    var c_code = document.querySelector('.c_code').value;
+    var phone = document.querySelector('.phone').value;
+    var email = document.querySelector('.email').value;
+    var c_name = document.querySelector('.c_name').value;
+    var country = document.querySelector('.country').value;
+    var someCheckbox = document.querySelector('.term');
+
+    var new_mobile = c_code + " " + phone;
+    
+    var obj = {name,password,new_mobile,email,c_name,country};
+    user_data.push(obj);
+
+   localStorage.setItem('user_details',JSON.stringify(user_data));
+    
+})
